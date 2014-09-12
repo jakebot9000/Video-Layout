0101a62ec7fb49385d5dd207c56bb4571528fef4
jakebot9000 <jacobchalkley@gmail.com>
2014-09-12 16:26:04 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,149 @@ var adState = 'pre_loader'; //pre_loader,panel_intro,panel_one,panel_two_etc...
 var ytp_intro = null; //intro YTP instance
 var ytpInteriorConfig = [];
 var ytpInteriorPlayers = [];
-var ytp_interior1; //interior YTP 1 instance
-var ytp_interior2;
+var ytp_interior1 = null; //interior YTP 1 instance
+var ytp_interior2 = null;
 var firstIntroPlay = true;
-var isVideoPlaying = false;
+var videoState = {};
+
+
+// LAYOUTS
+// Configurable API.
+/* TODO:
+    - Finalize Filler class structure
+    - Hook up Filler values to actual components
+    -
+*/
+var configurable,
+    config,
+    root,
+    binding;
+
+studio.utils.EnablerAccessor.loadModuleWhenReady(
+    studio.module.ModuleId.CONFIGURABLE_FILLER, loadModuleHandler);
+
+function loadModuleHandler() {
+  window.console.log('loadModuleHandler');
+  configurable = studio.sdk.configurable;
+  config = configurable.config;
+  binding = configurable.binding;
+
+  config.declare('YouTubeCloseButton', {
+    'language': 'en', //see cs/ for full list
+    'shadow': true,
+    'theme': {
+      '@value': 'black', //white,black,gray
+      '@required': false
+    }
+  });
+
+  config.declare('YouTubePlayer', {
+    'videoId': '123456',
+    'videoWidth': 300,
+    'videoHeight': 200
+  });
+
+  //JAKE!
+  config.declare('PanelBackground', {
+    'backgroundImage': {
+      '@type': 'color' //JAKE! Why doesn't image work in local mode??
+    }
+  });
+
+  // Declare main configuration.
+  config.declare('Main', {
+    'closeButton': { '@type': 'YouTubeCloseButton' },
+    'introVideo': { '@type': 'YouTubePlayer' },
+    'background': { '@type': 'PanelBackground' }
+  });
+
+  // Instantiate.
+  var configuration = config.instantiate('Main', {
+    'closeButton': {
+      'shadow': true
+    },
+    'introVideo': {
+      'videoId': 'ibzGjdcNGXM',
+      'videoWidth': 1110,
+      'videoHeight': 250,
+    }
+    // JAKE! Ask Paul about this
+    // 'background': {
+    //   'backgroundImage': {
+    //     '@type': '#ffffff'
+    //   }
+    // }
+  });
+
+  // Register.
+  configurable.register(configuration, registerHandler);
+}
+
+function registerHandler(object) {
+  root = object;
+  console.log(root);
+  window.console.log('registerHandler');
+  window.console.log('Config runtime mode: '+configurable.getRuntimeMode());
+
+  // Display local filler. Don't upload to Studio as is!!!
+  // DEVELOPMENT - local testing
+  // FILLER - uploaded to studio and filler view is visible
+  // PLAY - in studio clicking play button which hides filler view
+  // TRAFFICK - trafficked and live??
+  if(configurable.Filler &&
+     configurable.getRuntimeMode() == configurable.RUNTIME_MODE.DEVELOPMENT) {
+    window.console.log('local developer mode!');
+
+    var div = document.createElement('div');
+    div.id = 'filler';
+    window.parent.document.body.appendChild(div);
+    configurable.Filler.display(div, object);
+
+  }else {
+    window.console.log('I am in studio!');
+  }
+
+  // create initial close button based on Filler defaults
+  setupCloseBtn(root['closeButton']);
+
+  // any changes on object trigger this
+  binding.addValueChangeListener(root, valueChangeListener);
+  // any changes on foo trigger this
+  binding.addValueChangeListener(root['closeButton'], ytCloseBtnListener);
+  // any changes on image upload trigger this
+  //binding.addPropertyChangeListener(root['IntroYouTube'], 'videoId', ytIdChange);
+
+  //JAKE!
+  binding.addPropertyChangeListener(
+    root['Main'],
+    'background',
+    this.backgroundListener,
+    this);
+
+}
+
+function valueChangeListener(value) {
+  console.log('valueChangeListener', value);
+
+}
+
+function ytCloseBtnListener(value) {
+  console.log('ytCloseBtnListener', value);
+  setupCloseBtn(root['closeButton']);
+}
+
+function ytIdChange(value) {
+  console.log('ytIdChange', value);
+}
+
+function backgroundListener(value) {
+  console.log('background', value);
+  var panelone = document.querySelector('#panel_one');
+  panelone.style.backgroundColor=value;
+  //"url('"+value+"')";
+
+}
+
 
 // Some Layout object testing here
 var ytpIntroConfig =  {
@@ -79,7 +218,10 @@ var politeInit = function() {
   panel.preload = document.querySelector('#pre_loader');
   panel.intro = document.querySelector('#panel_intro');
   panel.one = document.querySelector('#panel_one');
+  button.introSet = document.querySelector('#intro_controls');
   button.skip = document.querySelector('#intro_controls .skip');
+  button.introPlayToggle = document.querySelector('#intro_controls .playToggle');
+  button.introVolumeToggle = document.querySelector('#intro_controls .volumeToggle');
   button.video1Button = document.querySelector('.select_buttons .video1');
   button.video2Button = document.querySelector('.select_buttons .video2');
 
@@ -150,19 +292,67 @@ var attachEvents = function() {
     return ;
   }
 
+  button.introPlayToggle.addEventListener('click',introPlayToggle);
+  function introPlayToggle() {
+    if(videoState.intro.playing) {
+      Enabler.counter('Video Intro : Pause');
+      ytp_intro.pauseVideo();
+    } else {
+      Enabler.counter('Video Intro : Play');
+      ytp_intro.playVideo();
+    }
+
+    return ;
+  }
+
+  button.introVolumeToggle.addEventListener('click',introVolumeToggle);
+  function introVolumeToggle() {
+    if(!videoState.intro.muted) {
+      Enabler.counter('Video Intro : Mute');
+      ytp_intro.mute();
+      videoState.intro.muted = true;
+    } else {
+      Enabler.counter('Video Intro : UnMute');
+      ytp_intro.unMute();
+      videoState.intro.muted = false;
+    }
+
+    return ;
+  }
+
+
   var refreshButton = document.getElementsByClassName('refresh_ad');
   refreshButton[0].onclick = refreshAd;
   function refreshAd() {
     Enabler.counter('Button : Replay Intro');
 
     pauseAllVideos();
-
     ytp_intro.unMute();
+    videoState.intro.muted = false;
     transitionToState('panel_intro');
     return ;
   }
 
 };
+
+
+/*
+<ci-ytclosebutton lang="en" theme="black" shadow="true" id="ytClose_dc"/></ci>
+*/
+var setupCloseBtn = function(e) {
+  window.console.log('setupCloseBtn: '+e.theme);
+  var closeElement = document.querySelector('#ytClose_dc');
+  if(closeElement) {
+    document.querySelector('#ad_container').removeChild(closeElement);
+  }
+  closeElement = document.createElement('ci-ytclosebutton');
+  closeElement.id = 'ytClose_dc';
+  closeElement.lang = e.language;
+  closeElement.theme = e.theme;
+  closeElement.shadow = e.shadow;
+  closeElement.style.display = 'block';
+  document.querySelector('#ad_container').appendChild(closeElement);
+}
 
 
 var transitionToState = function(state) {
@@ -179,14 +369,17 @@ var transitionToState = function(state) {
 
       // Hide Pre-Load Section when the Intro Video Starts Playing
       // Keep player hidden until buffer
+      button.introSet.style.display = 'none';
       panel.intro.style.display = "block";
       panel.preload.style.display = "none";
       panel.one.style.display = "none";
-      if(firstIntroPlay) { ytp_intro.mute(); }
+      if(firstIntroPlay) {
+        ytp_intro.mute();
+        videoState.intro.muted = true;
+      }
       // Need to restart at 0 on replay however seekTo method is not an exposed
       // method in IS component. Using loadVideoById instead of playVideo
       ytp_intro.loadVideoById(ytpIntroConfig['videoId']);
-
       firstIntroPlay = false;
     break;
     case "panel_one":
@@ -198,6 +391,7 @@ var transitionToState = function(state) {
 
 
 var setupIntroYTP = function() {
+  videoState.intro = {};
 
   // Create separate iframe div to hold player so wrapper container not
   // converted to iframe
@@ -293,10 +487,10 @@ var onPlayerStateChange = function(stateChangeEvent) {
   switch(playerState){
     case studioinnovation.YTPlayer.Events.ENDED:
       //console.log('END');
-      isVideoPlaying = false;
 
       switch(caller) {
         case 'ytp_intro':
+          videoState.intro.playing = false;
           transitionToState('panel_one');
         break;
         case 'ytp_1':
@@ -308,13 +502,22 @@ var onPlayerStateChange = function(stateChangeEvent) {
       }
 
     break;
-    case studioinnovation.YTPlayer.Events.PLAYING:
+    case studioinnovation.YTPlayer.Events.PLAYING: // Not fired on loadVideoById
       console.log('PLAYING');
-      isVideoPlaying = true;
+      switch(caller) {
+        case 'ytp_intro':
+          videoState.intro.playing = true;
+          console.log("videoState.intro.playing: "+videoState.intro.playing);
+        break;
+      }
     break;
     case studioinnovation.YTPlayer.Events.PAUSED:
-      //console.log('PAUSED');
-      isVideoPlaying = false;
+      switch(caller) {
+        case 'ytp_intro':
+          videoState.intro.playing = false;
+          console.log("videoState.intro.playing: "+videoState.intro.playing);
+        break;
+      }
     break;
     case studioinnovation.YTPlayer.Events.BUFFERING:
       //console.log('BUFFERING');
@@ -329,6 +532,13 @@ var onPlayerStateChange = function(stateChangeEvent) {
      break;
     case  studioinnovation.YTPlayer.Events.TIMER_START:
       //console.log('TIMER START')
+      switch(caller) {
+        case 'ytp_intro':
+          //show controls on play
+          button.introSet.style.display = 'block';
+          videoState.intro.playing = true; // make sure playing state set properly
+        break;
+      }
      break;
     case  studioinnovation.YTPlayer.Events.TIMER_STOP:
       //console.log('TIMER END')
